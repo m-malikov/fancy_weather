d177d1fbd5889282ab5466bf2779b5b36713ade2
Matvey Volkov <m.b.volkov@yandex.ru>
2020-05-04 01:43:11 +0300

    imgur

--- pictures/pic_storage.Dockerfile
+++
@@ -1,8 +0,0 @@
-# docker build --file=pictures/pic_storage.Dockerfile --tag=salamantos/fancy_weather:pic_storage-1.0.0 .
-FROM python:3.7-slim
-
-# Set working directory
-WORKDIR /usr/app
-
-# Add pictures
-COPY ./pictures/pic_storage ./pic_storage
--- pictures/tests/test_app.py
+++ pictures/tests/test_app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from pictures.generator import generate_picture
+from pictures import generate_picture, init_available_images_collection
 
 """
 Documentation: https://docs.pytest.org
@@ -15,43 +15,38 @@ Useful flags: '-v' for verbose, '-l' for variables printing at error
 class TestPictureGenerator:
     def setup_class(self):
         self.application = Flask('pictures')
+        self.images_links = init_available_images_collection(self.application)
 
     def test_wrong_weather(self):
         with self.application.app_context():
-            assert generate_picture('wrong_weather_type') is None
+            assert generate_picture('wrong_weather_type', self.images_links) is None
 
     def test_rainy(self):
         with self.application.app_context():
-            pic_path = generate_picture('rainy')
-            assert pic_path is not None
-            assert 'rainy' in pic_path
+            image_link = generate_picture('rainy', self.images_links)
+            assert image_link is not None
 
     def test_snowy(self):
         with self.application.app_context():
-            pic_path = generate_picture('snowy')
-            assert pic_path is not None
-            assert 'winter' in pic_path
+            image_link = generate_picture('snowy', self.images_links)
+            assert image_link is not None
 
     def test_cold(self):
         with self.application.app_context():
-            pic_path = generate_picture('cold')
-            assert pic_path is not None
-            assert 'winter' in pic_path
+            image_link = generate_picture('cold', self.images_links)
+            assert image_link is not None
 
     def test_warm(self):
         with self.application.app_context():
-            pic_path = generate_picture('warm')
-            assert pic_path is not None
-            assert 'summer' in pic_path
+            image_link = generate_picture('warm', self.images_links)
+            assert image_link is not None
 
     def test_spring(self):
         with self.application.app_context():
-            pic_path = generate_picture('spring')
-            assert pic_path is not None
-            assert 'spring' in pic_path
+            image_link = generate_picture('spring', self.images_links)
+            assert image_link is not None
 
     def test_autumn(self):
         with self.application.app_context():
-            pic_path = generate_picture('autumn')
-            assert pic_path is not None
-            assert 'autumn' in pic_path
+            image_link = generate_picture('autumn', self.images_links)
+            assert image_link is not None
